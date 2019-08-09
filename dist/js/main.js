b18784e81fbc53ff24446580d694b41c2c7fe21c
Taras Kharuk <tkharuk@kountable.com>
2019-08-09 11:29:39 +0300

    built from source v0.95

---
+++ dist/js/main.js
@@ -0,0 +1,20 @@
+/*!
+ * altigee.github.io v1.0.0
+ * Altigee website
+ * (c) 2019 
+ * MIT License
+ * 
+ */
+
+// console.log('%c🚧 Website is under construction 🚧', "font-size: 32px");
+
+const menuContainer = document.querySelector('.main-navigation-mobile');
+const menuOpenBtn = menuContainer.querySelector('.menu-toggle-btn');
+const menuCloseBtn = menuContainer.querySelector('.menu-close-btn');
+
+menuOpenBtn.addEventListener('click', toggleMenu);
+menuCloseBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  menuContainer.classList.toggle('opened');
+}

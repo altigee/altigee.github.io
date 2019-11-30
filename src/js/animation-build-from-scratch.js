ba9aa9a49fb67fbe324bea67f30dc9a4932a7f6b
oskyriienko <oskyriienko@gmail.com>
2019-11-30 11:27:43 +0200

    add animation

--- src/js/animation-build-from-scratch.js
+++ src/js/animation-build-from-scratch.js
@@ -7,3 +7,48 @@ teamBGImg.src =
 teamBGImg.onload = function() {
   teamBG.classList.add('animate-bg-fade-in');
 };
+
+const scroll =
+  window.requestAnimationFrame ||
+  // IE Fallback
+  function(callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+
+const get = document.querySelectorAll('.scroll-rotate');
+const phase_items = document.querySelectorAll('.phase-steps');
+const phase_items_parent = document.querySelector('.phase-steps').closest('ul');
+
+function loop() {
+  if (isElementInViewport(phase_items_parent)) {
+    phase_items.forEach(function(element) {
+      element.classList.add('animate-phase-steps');
+    });
+  }
+
+  get.forEach(function(element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('animate-rotate');
+    } else {
+      element.classList.remove('animate-rotate');
+    }
+  });
+
+  scroll(loop);
+}
+
+loop();
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
--- src/js/animation-team-extend.js
+++ src/js/animation-team-extend.js
@@ -1,6 +1,5 @@
 const teamBG = document.querySelector('.extend-bg');
 const teamBGImg = new Image();
-
 teamBGImg.src =
   teamBG.clientWidth > 991
     ? '../images/backgrounds/team-extend-desktop.jpg'
@@ -8,3 +7,50 @@ teamBGImg.src =
 teamBGImg.onload = function() {
   teamBG.classList.add('animate-bg-fade-in');
 };
+
+const scroll =
+  window.requestAnimationFrame ||
+  // IE Fallback
+  function(callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+
+const get = document.querySelectorAll('.scroll-rotate');
+const phase_consistent = document.querySelectorAll('.phase-consistent');
+const phase_consistent_parent = document
+  .querySelector('.phase-consistent')
+  .closest('ul');
+
+function loop() {
+  if (isElementInViewport(phase_consistent_parent)) {
+    phase_consistent.forEach(function(element) {
+      element.classList.add('animate-phase-consistent');
+    });
+  }
+
+  get.forEach(function(element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('animate-rotate');
+    } else {
+      element.classList.remove('animate-rotate');
+    }
+  });
+
+  scroll(loop);
+}
+
+loop();
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
